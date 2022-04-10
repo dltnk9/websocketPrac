4b32f611e64270654cb38629e7c21f95a68d331e
dltnk9 <dltnk9@naver.com>
2022-04-10 20:50:46 +1000

    web socket prac

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http")
+const app = express();
+const path = require("path")
+const server = http.createServer(app)
+const socketIO = require("socket.io")
+
+const io = socketIO(server);
+
+
+console.log(__dirname)
+console.log("hello")
+app.use(express.static(path.join(__dirname, "src")));
+
+io.on("connection", (socket) => {
+  socket.on("chatting", (data) => {
+    console.log(data)
+    io.emit("chatting", data)
+
+  })
+})
+
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`server is running${PORT}`) )
